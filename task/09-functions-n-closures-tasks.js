974250f6106dcfbf67dead7cb006dedf93684741
Burkun <duno3avr@gmail.com>
2019-04-14 17:53:25 +0300

    completed some tasks

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -26,7 +26,9 @@
  *
  */
 function getComposition(f,g) {
-    throw new Error('Not implemented');
+  return function(x) {
+      return f(g(x));
+  }
 }
 
 
@@ -47,7 +49,9 @@ function getComposition(f,g) {
  *
  */
 function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
+  return function(x){
+      return Math.pow(x,exponent);
+  }
 }
 
 
@@ -65,7 +69,17 @@ function getPowerFunction(exponent) {
  *   getPolynom()      => null
  */
 function getPolynom() {
-    throw new Error('Not implemented');
+  if (arguments.length > 0) {
+      var args = arguments;
+      return function(x) {
+          var sum = 0;
+          for (var i = 0; i < args.length; i++)
+              sum += args[i] * Math.pow(x, args.length - i - 1);
+          return sum;
+      };
+  }
+  else
+      return null;
 }
 
 
@@ -104,7 +118,17 @@ function memoize(func) {
  * retryer() => 2
  */
 function retry(func, attempts) {
-    throw new Error('Not implemented');
+  return function() {
+    var retries = attempts;
+    while (retries >= 0) {
+        try {
+            return func();
+        }
+        catch (ex) {
+            retries--;
+        }
+    }
+  };
 }
 
 
@@ -150,7 +174,7 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn) {
-    throw new Error('Not implemented');
+  throw new Error('Not implemented');
 }
 
 
@@ -171,7 +195,7 @@ function partialUsingArguments(fn) {
  *   getId10() => 11
  */
 function getIdGeneratorFunction(startFrom) {
-    throw new Error('Not implemented');
+  return () => startFrom++;
 }
 
 
